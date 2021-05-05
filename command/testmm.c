85f60e0138572c3e2d042a58561248a06846a54a
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-05 23:41:30 +0800

    Add files via upload

---
+++ command/testmm.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: l
+ * @Date: 2021-05-05 22:28:56
+ * @LastEditors: l
+ * @LastEditTime: 2021-05-05 22:28:57
+ * @FilePath: \KusaOS\KusaOS\command\testmm.c
+ */
+#include "type.h"
+#include "stdio.h"
+
+int main(int argc, char * argv[])
+{
+	printf("command testmm\n");
+	return 0;
+}
