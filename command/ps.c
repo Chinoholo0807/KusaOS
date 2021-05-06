8931e1392730a25cd989fb83ec0849b8764e0e0f
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-07 00:53:28 +0800

    Create ps.c

---
+++ command/ps.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: l
+ * @Date: 2021-05-07 00:07:06
+ * @LastEditors: l
+ * @LastEditTime: 2021-05-07 00:07:21
+ * @FilePath: \KusaOS-mid\command\ps.c
+ */
+#include "type.h"
+#include "stdio.h"
+
+int main(int argc, char * argv[])
+{
+	pstat();
+	return 0;
+}
