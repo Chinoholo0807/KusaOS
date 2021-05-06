89a47dc150cbffb465c9da48cffe0925505470c9
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-06 20:22:42 +0800

    Add files via upload

---
+++ command/cat.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: l
+ * @Date: 2021-05-05 22:25:07
+ * @LastEditors: l
+ * @LastEditTime: 2021-05-05 23:32:24
+ * @FilePath: \KusaOS\KusaOS\command\cat.c
+ */
+#include "type.h"
+#include "stdio.h"
+
+int main(int argc, char * argv[])
+{
+	printf("command cat\n");
+	return 0;
+}
---
+++ command/cd.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: l
+ * @Date: 2021-05-05 22:22:27
+ * @LastEditors: l
+ * @LastEditTime: 2021-05-05 22:22:27
+ * @FilePath: \KusaOS\KusaOS\command\cd.c
+ */
+#include "type.h"
+#include "stdio.h"
+
+int main(int argc, char * argv[])
+{
+	printf("command cd\n");
+	return 0;
+}
