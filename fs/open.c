c4d7148bd13f7cacf5b066f492917f8989710109
Big_K <54524841+BuptMurloc@users.noreply.github.com>
2021-05-07 19:13:01 +0800

    Update open.c
    增加了do_ls()和do_mkdir()，在create_file()增加“根据flags来区分创建普通文件和目录文件”的功能，给new_inode加了个参数

--- fs/open.c
+++ fs/open.c
@@ -22,11 +22,96 @@
 #include "keyboard.h"
 #include "proto.h"
 
+#define ISDIR -66378
+
 PRIVATE struct inode * create_file(char * path, int flags);
 PRIVATE int alloc_imap_bit(int dev);
 PRIVATE int alloc_smap_bit(int dev, int nr_sects_to_alloc);
-PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect);
+PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect, int imode);
 PRIVATE void new_dir_entry(struct inode * dir_inode, int inode_nr, char * filename);
+
+/*****************************************************************************
+ *                                do_ls
+ *****************************************************************************/
+PUBLIC int do_ls(){
+	char pathname[MAX_PATH];
+
+   	/* get parameters from the message */
+   	int flags = fs_msg.FLAGS;   /* access mode */
+   	int name_len = fs_msg.NAME_LEN; /* length of filename */
+   	int src = fs_msg.source;    /* caller proc nr. */
+   	assert(name_len < MAX_PATH);
+
+   	phys_copy((void*)va2la(TASK_FS, pathname),
+        (void*)va2la(src, fs_msg.PATHNAME),
+	name_len);
+    	pathname[name_len] = 0;
+
+	int i,j;
+
+	struct inode * dir_inode;
+    	char filename[20];
+    	strip_path(filename, pathname,&dir_inode);
+
+	int dir_blk0_nr = dir_inode->i_start_sect;
+   	int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+    	int nr_dir_entries = dir_inode->i_size / DIR_ENTRY_SIZE;
+    	int m = 0;
+
+	struct dir_entry * pde;
+
+	printl("\ninode        filename\n");
+    	printl("============================\n");
+
+    	for (i = 0; i < nr_dir_blks; i++){
+        	RD_SECT(dir_inode->i_dev, dir_blk0_nr + i);
+		pde = (struct dir_entry *)fsbuf;
+        		for (j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++, pde++){
+            			/*struct inode *n = find_inode(pde->inode_nr);*/
+            			printl("  %2d        %s\n", pde->inode_nr , pde->name);
+            			if (++m >= nr_dir_entries){
+                			printl("\n");
+                			break;
+            			}
+        		}
+        	if (m > nr_dir_entries) //[> all entries have been iterated <]
+            	break;
+    	}	
+
+    	printl("============================\n");
+	return 0;
+}
+
+/*****************************************************************************
+ *                                do_mkdir
+ *****************************************************************************/
+PUBLIC int do_mkdir(){
+	char pathName[MAX_PATH];
+	
+	// 取得message中的信息
+	int flages = fs_msg.FLAGS;
+	int name_len = fs_msg.NAME_LEN;
+	int source = fs_msg.source;
+	assert(name_len < MAX_PATH);  // 路径名称长度不得超过最大长度
+	
+	phys_copy((void*)va2la(TASK_FS, pathName), (void*)va2la(source, fs_msg.PATHNAME), name_len);
+    	pathName[name_len] = 0;
+
+	struct inode* dir_inode = create_file(pathName, ISDIR);
+	if (dir_inode)
+	{
+		printl("creating directory %s succeeded!\n", pathName);
+		put_inode(dir_inode);
+		return 0;
+	}
+	else
+	{
+		printl("creating directory %s failed!\n", pathName);
+		return -1;
+	}
+	
+	return 0;
+}
 
 /*****************************************************************************
  *                                do_open
@@ -117,7 +202,7 @@ PUBLIC int do_open()
 				  &driver_msg);
 		}
 		else if (imode == I_DIRECTORY) {
-			assert(pin->i_num == ROOT_INODE);
+			//assert(pin->i_num == ROOT_INODE);
 		}
 		else {
 			assert(pin->i_mode == I_REGULAR);
@@ -154,10 +239,15 @@ PRIVATE struct inode * create_file(char * path, int flags)
 	int inode_nr = alloc_imap_bit(dir_inode->i_dev);
 	int free_sect_nr = alloc_smap_bit(dir_inode->i_dev,
 					  NR_DEFAULT_FILE_SECTS);
-	struct inode *newino = new_inode(dir_inode->i_dev, inode_nr,
-					 free_sect_nr);
-
-	new_dir_entry(dir_inode, newino->i_num, filename);
+	struct inode *newino;
+	if (flags == ISDIR){
+		newino = new_inode(dir_inode->i_dev, inode_nr, free_sect_nr, I_DIRECTORY);
+		new_dir_entry(dir_inode, newino->i_num, filename);
+	}
+	else{
+		newino = new_inode(dir_inode->i_dev, inode_nr, free_sect_nr, I_REGULAR);
+		new_dir_entry(dir_inode, newino->i_num, filename);
+	}
 
 	return newino;
 }
@@ -335,11 +425,11 @@ PRIVATE int alloc_smap_bit(int dev, int nr_sects_to_alloc)
  * 
  * @return  Ptr of the new i-node.
  *****************************************************************************/
-PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect)
+PRIVATE struct inode * new_inode(int dev, int inode_nr, int start_sect, int imode)
 {
 	struct inode * new_inode = get_inode(dev, inode_nr);
 
-	new_inode->i_mode = I_REGULAR;
+	new_inode->i_mode = imode;
 	new_inode->i_size = 0;
 	new_inode->i_start_sect = start_sect;
 	new_inode->i_nr_sects = NR_DEFAULT_FILE_SECTS;
