9a315e289da833978d6712a0047056b579f20c4b
Big_K <54524841+BuptMurloc@users.noreply.github.com>
2021-05-07 21:10:13 +0800

    Update fs.c
    写了ls,touch,rm,mkdir

--- command/fs.c
+++ command/fs.c
@@ -1,8 +1,82 @@
 #include "stdio.h"
 #include "type.h"
+#include "string.h"
 int main(int argc, char * argv[])
 {
-	printf("command fs\n");
+	if(argc <= 2){
+		printf("ERROR PARAMETAR NUM\n");
+		return -1;
+	}
+	else if(strcmp(argv[1],"ls")==0){
+		printf("command ls\n");
+		ls("/");
+	}
+	else if(strcmp(argv[1],"mkdir")==0){
 
+		printf("command mkdir\n");
+
+		if(argc <= 3){
+                	printf("ERROR PARAMETAR NUM\n");
+                	return -1;
+        	}
+
+		int fd = open(argv[2], O_RDWR);
+    		if (fd != -1){
+        		printf("Failed to create a new directory with name %s\n", argv[2]);
+			close(fd);
+        		return -1;
+    		}
+		mkdir(argv[2]);
+	}
+	else if(strcmp(argv[1],"touch")==0){
+		
+		printf("command touch\n");
+
+		if(argc <= 3){
+			printf("ERROR PARAMETAR NUM\n");
+                        return -1;
+		}
+		
+		int fd = open(argv[2], O_RDWR);
+
+   		if (fd != -1){
+        		printf("Failed to create a new file with name %s\n", argv[2]);
+			close(fd);
+        		return -1;
+    		}
+	
+		fd = open(argv[2], O_CREAT);
+	
+		if(fd == -1){
+			printf("Failed to create a new file with name %s\n", argv[2]);
+        	    	return -1;	
+		}
+
+		printf("File created: %s \n", argv[2]);
+		
+        }
+	else if(strcmp(argv[1],"rm")==0){
+		printf("command rm\n");
+
+                if(argc <= 3){
+                        printf("ERROR PARAMETAR NUM\n");
+                        return -1;
+                }
+
+		int fd = open(argv[2], O_RDWR);
+
+        	if (fd == -1){
+                	printf("Failed to delete a file with name %s\n", argv[2]);
+              		return -1;
+        	}
+
+		close(fd);
+
+		unlink(argv[2]);
+
+        }
+	else {
+		printf("WRONG COMMAND\n");
+	}
 	return 0;
 }
