a583580631d14fe0c10764f379e9f8a2315909d6
Big_K <54524841+BuptMurloc@users.noreply.github.com>
2021-05-08 21:12:38 +0800

    Update fs.c
    严格化了参数个数，适配绝对路径和相对路径

--- command/fs.c
+++ command/fs.c
@@ -8,36 +8,69 @@ int main(int argc, char * argv[])
 		return -1;
 	}
 	else if(strcmp(argv[1],"ls")==0){
-		printf("command ls\n");
-		ls("/");
+		//printf("command ls\n");
+
+		if(argc != 3){
+			printf("WRONG PARAMETAR NUM\n");
+			return -1;
+		}
+
+		char abs_path[MAX_PATH];
+		
+                memcpy(abs_path,argv[2],MAX_PATH);
+
+		//printf("target = %s\n",argv[2]);	
+		//printf("{ls:} %s\n",abs_path);
+		ls(abs_path);
+		
 	}
 	else if(strcmp(argv[1],"mkdir")==0){
 
-		printf("command mkdir\n");
+		//printf("command mkdir\n");
 
-		if(argc <= 3){
+		if(argc != 4){
                 	printf("WRONG PARAMETAR NUM\n");
                 	return -1;
         	}
 
-		int fd = open(argv[2], O_RDWR);
+		char abs_path[MAX_PATH];
+		
+		memcpy(abs_path,argv[3],MAX_PATH);
+                if(strlen(abs_path) != 1){
+                        strcat(abs_path,"/");
+                }
+                strcat(abs_path,argv[2]);
+
+                printf("abs_path = %s\n",abs_path);
+
+		int fd = open(abs_path, O_RDWR);
     		if (fd != -1){
         		printf("Failed to create a new directory with name %s\n", argv[2]);
 			close(fd);
         		return -1;
     		}
-		mkdir(argv[2]);
+		mkdir(abs_path);
 	}
 	else if(strcmp(argv[1],"touch")==0){
 		
-		printf("command touch\n");
+		//printf("command touch\n");
 
-		if(argc <= 3){
+		if(argc != 4){
 			printf("WRONG PARAMETAR NUM\n");
                         return -1;
 		}
+
+		char abs_path[MAX_PATH];
+
+		memcpy(abs_path,argv[3],MAX_PATH);
+		if(strlen(abs_path) != 1){
+			strcat(abs_path,"/");
+		}
+		strcat(abs_path,argv[2]);
 		
-		int fd = open(argv[2], O_RDWR);
+		//printf("abs_path = %s\n",abs_path);
+			
+		int fd = open(abs_path, O_RDWR);
 
    		if (fd != -1){
         		printf("Failed to create a new file with name %s\n", argv[2]);
@@ -45,7 +78,7 @@ int main(int argc, char * argv[])
         		return -1;
     		}
 	
-		fd = open(argv[2], O_CREAT);
+		fd = open(abs_path, O_CREAT);
 	
 		if(fd == -1){
 			printf("Failed to create a new file with name %s\n", argv[2]);
@@ -56,14 +89,24 @@ int main(int argc, char * argv[])
 		
         }
 	else if(strcmp(argv[1],"rm")==0){
-		printf("command rm\n");
+		//printf("command rm\n");
 
-                if(argc <= 3){
+                if(argc != 4){
                         printf("WRONG PARAMETAR NUM\n");
                         return -1;
                 }
+		
+		char abs_path[MAX_PATH];
 
-		int fd = open(argv[2], O_RDWR);
+		memcpy(abs_path,argv[3],MAX_PATH);
+		if(strlen(abs_path) != 1){
+			strcat(abs_path,"/");
+		}
+		strcat(abs_path,argv[2]);
+
+		//printf("abs_path = %s\n",abs_path);
+
+		int fd = open(abs_path, O_RDWR);
 
         	if (fd == -1){
                 	printf("Failed to delete a file with name %s\n", argv[2]);
@@ -72,7 +115,7 @@ int main(int argc, char * argv[])
 
 		close(fd);
 
-		unlink(argv[2]);
+		unlink(abs_path);
 
         }
 	else {
