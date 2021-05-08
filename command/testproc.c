896a112e0a0f1ad0c78413b1a058cae4e57e30bc
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-08 15:17:12 +0800

    Update testproc.c

--- command/testproc.c
+++ command/testproc.c
@@ -11,6 +11,14 @@
 
 int main(int argc, char * argv[])
 {
-	printf("command testproc\n");
+	int i;
+	for (i = 1; i < argc; i++)
+		printf("%s%s", i == 1 ? "" : " ", argv[i]);
+	printf("\n");
+	int ret = change_schedule_policy(4);
+		printf("current scheduel policy:%d",ret);
+	change_proc_priority(5,10);
+	change_proc_priority(6,11);
+	change_proc_priority(7,12);
 	return 0;
 }
