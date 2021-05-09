3f88b67c1e11283c3f95e9f15d64eba519b74bdd
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-09 13:13:08 +0800

    Update csche.c

--- command/csche.c
+++ command/csche.c
@@ -11,31 +11,19 @@
 
 int main(int argc, char * argv[])
 {
-	if(argc !=2){
+    if(argc !=2){
         printf("args error,pleace input [ csche NR_POLICY ]\n");
         printf("1 -- SCHED_RR  2 -- SCHED_FIFO\n");
         printf("3 -- SCHED_PRI 4 -- SCHED_PRI_DY\n");
     }else{
         int nr_policy =0;
-        pid = argv[1][0]-'0';
-        int result = kill_process(pid);
-        switch(result){
-          case 1:
-            printf("Current schedule policy: SHCED_RR\n");
-            break;
-          case 2:
-            printf("Current schedule policy: SHCED_FIFO\n");
-            break;
-          case 3:
-            printf("Current schedule policy: SHCED_PRI\n");
-            break;
-          case 4:
-            printf("Current schedule policy: SHCED_PRI_DY\n");
-            break;  
-          default:
-            printf("Error return result:%d\n",result);
-            break;
-         }
+        nr_policy = argv[1][0]-'0';
+        int result = change_schedule_policy(nr_policy);
+        char *schedule_name[]={"","SCHED_RR","SCHED_FIFO","SCHED_PRI","SCHED_PRI_DY"}
+	if( result>0&& result<=4)
+		printf("Current schedule policy:%s\n",schedule_name[reslut]);
+	else
+		printf("Error,change schedule policy failed\n");
     }
 	return 0;
 }
