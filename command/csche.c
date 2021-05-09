9a24ed06149cbdf131db32e777dcde5a3fa57c9c
Haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2021-05-09 13:35:52 +0800

    Update csche.c

--- command/csche.c
+++ command/csche.c
@@ -19,9 +19,9 @@ int main(int argc, char * argv[])
         int nr_policy =0;
         nr_policy = argv[1][0]-'0';
         int result = change_schedule_policy(nr_policy);
-        char *schedule_name[]={"","SCHED_RR","SCHED_FIFO","SCHED_PRI","SCHED_PRI_DY"}
+        char *schedule_name[]={"","SCHED_RR","SCHED_FIFO","SCHED_PRI","SCHED_PRI_DY"};
 	if( result>0&& result<=4)
-		printf("Current schedule policy:%s\n",schedule_name[reslut]);
+		printf("Current schedule policy:%s\n",schedule_name[result]);
 	else
 		printf("Error,change schedule policy failed\n");
     }
